ad018fe8048619299544fbe9e711d3a333af7966
sugyeongsu <tnrud321@gmail.com>
2023-08-22 23:28:53 +0900

    Update index.js

--- codecamp-frontend-susu/my-class/pages/index.js
+++ codecamp-frontend-susu/my-class/pages/index.js
@@ -14,3 +14,16 @@ export default function Home() {
     </>
   )
 }
+
+
+function Home(){
+	return (
+		<div className={styles.wrapper}>
+			<h1>로그인</h1>
+			<label className={styles.label}>아이디</label>
+			<input className={styles.input} type="text" />
+			<label className={styles.label}>비밀번호</label>
+			<input className={styles.input} type="password" />
+		</div>
+	)
+}
