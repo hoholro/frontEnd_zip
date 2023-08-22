2c09c71e130666efea678995ff3c1be0d1157efd
sugyeongsu <tnrud321@gmail.com>
2023-08-23 00:19:18 +0900

    ㅇㅇ

--- camp/my-class/pages/index.js
+++ camp/my-class/pages/index.js
@@ -10,7 +10,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>밍밍이와 함께하는 리액트 매직쇼</div>
+      <div className={styles.wrapper}>
+			<h1>로그인</h1>
+			<label className={styles.label}>아이디</label>
+			<input className={styles.input} type="text" />
+			<label className={styles.label}>비밀번호</label>
+			<input className={styles.input} type="password" />
+		</div>
+
+    <div>왜안보임..</div>
     </>
   )
 }
